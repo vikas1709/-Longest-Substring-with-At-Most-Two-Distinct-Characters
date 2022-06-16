f566ca713e0735eeb90b99bcdb9a23332cd05c59
vikas1709 <vc935@nyu.edu>
2022-06-16 04:33:14 -0400

    Update Longest-Substring-with-At-Most-Two-Distinct-Characters.cs

--- Longest-Substring-with-At-Most-Two-Distinct-Characters.cs
+++ Longest-Substring-with-At-Most-Two-Distinct-Characters.cs
@@ -1,3 +1,19 @@
+/*
+Given a string s, return the length of the longest substring that contains at most two distinct characters.
+Example 1:
+Input: s = "eceba"
+Output: 3
+Explanation: The substring is "ece" which its length is 3.
+
+
+Example 2:
+Input: s = "ccaabbb"
+Output: 5
+Explanation: The substring is "aabbb" which its length is 5.
+
+*/
+
+
 public class Solution {
     public int LengthOfLongestSubstringTwoDistinct(string s) {
         Dictionary<char, int> window = new Dictionary<char, int>();
